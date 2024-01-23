17e519783c4462c875b59dc1ab4bab1b8e8254d2
Prince Pal <palprince3690@gmail.com>
2024-01-23 20:23:30 +0530

    data scraped

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { extractPrice } from '../utils';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 export async function scrapeAmazonProduct(url: string) {
     if (!url) return;
 
@@ -31,10 +31,53 @@ export async function scrapeAmazonProduct(url: string) {
         const currentPrice = extractPrice(
             $('.priceToPay span.a-price-whole'),
             $('a.size.base.a-color-price'),
-            $('.a-button-selected .a-color-base'),
-            $('.a-price.a-text-price')
+            $('.a-button-selected .a-color-base')
+            
         );
-        console.log(title, currentPrice);
+
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('.a-price.a-text-price span.a-offscreen'),
+            $('#listPrice'),
+            $('#priceblock_dealprice'),
+            $('.a-size-base.a-color-price')
+        );
+
+        const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+
+        const images = $ ('#imgB1KFront').attr('data-a-dynamic-image') ||
+        $ ('#landingImage').attr('data-a-dynamic-image') ||
+        '{}'
+
+        const imgUrls = Object.keys(JSON.parse(images));
+
+        const currency = extractCurrency($('.a-price-symbol'));
+
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+
+        const description = extractDescription($);
+
+        //console.log({title, currentPrice, originalPrice, outOfStock, imgUrls,currency, discountRate});
+
+        const data = {
+            url,
+            currency,
+            image : imgUrls[0],
+            title,
+            currentPrice : Number(currentPrice) || Number(originalPrice),
+            originalPrice : Number(originalPrice) || Number(currentPrice),
+            priceHistory : [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewsCount: 100,
+            stars: 4.5,
+            isOutOfStock: outOfStock,
+            description,
+            lowestPrice : Number(currentPrice) || Number(originalPrice),
+            highestPrice : Number(originalPrice) || Number(currentPrice),
+            averagePrice : Number(currentPrice) || Number(originalPrice),
+        }
+        return data;
     } catch (error) {
         throw new Error('Failed to scratch products: ${error.message}')
     }
